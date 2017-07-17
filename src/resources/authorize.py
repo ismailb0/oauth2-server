8b853b2402a85da3c475fe939a7f111620148917
Ismail Benmoussa <ismailbenmoussa@gmail.com>
2017-07-17 21:00:10 +0200

    Add confirm page

--- src/resources/authorize.py
+++ src/resources/authorize.py
@@ -5,7 +5,7 @@ Define the REST verbs relative to the authorize resource
 from flask.ext.restful import Resource
 from flask.json import jsonify
 from flask import render_template, request
-from repositories import ClientRepository
+from repositories import ClientRepository, UserRepository
 from server import oauth
 import logging
 
@@ -16,7 +16,9 @@ class AuthorizeResource(Resource):
     def get(*args, **kwargs):
         client_id = request.args.get('client_id')
         client = ClientRepository.get(client_id=client_id)
+        user = UserRepository.get(id=client.user_id)
         kwargs['client'] = client
+        kwargs['user'] = user
         return render_template('oauthorize.html', **kwargs)
 
     @staticmethod
